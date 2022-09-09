73ac7b1a22228884c9f07c8ece12ad0abe0c011a
Neesham <neeshamraghav0@gmail.com>
2022-09-09 14:47:47 +0530

    Google Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,31 @@
 import '../styles/globals.css'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 
   return (
       <>
 
-
-        <Html>
-          <Head>
-
-
-            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-            <Script strategy="lazyOnload">
-                {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
-            </Script>
-
-
-          </Head>
-          
-        </Html>
-
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
 
         <Navbar/>
         <Component {...pageProps} />
