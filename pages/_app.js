fa51e1bdc51c6e0c8c5390f7d3a6d2d81a457e0e
Neesham <neeshamraghav0@gmail.com>
2022-09-09 14:13:30 +0530

    Adding script tag into head section

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 function MyApp({ Component, pageProps }) {
 
@@ -8,23 +9,33 @@ function MyApp({ Component, pageProps }) {
       <>
 
 
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Html>
+          <Head>
 
-        <Script strategy="lazyOnload">
-            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-                });
-            `}
-        </Script>
-        
+
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+            <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+
+
+          </Head>
+          
+        </Html>
+
+
         <Navbar/>
         <Component {...pageProps} />
         <Footer className={'mt-20'}/>
-      </>
+    </>
   )
 
 }
